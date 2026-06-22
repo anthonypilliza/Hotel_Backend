88663752bb2b6459b00d09517fe9c63106789f90
anthonypilliza <anthonypilliza3@gmail.com>
2026-06-22 17:52:55 -0500

    Centralización de constantes de roles y estados por Juan Diaz

---
+++ app/config/constants.py
@@ -0,0 +1,8 @@
+# Roles del sistema
+ROL_ADMIN = "Administrador"
+ROL_RECEPCIONISTA = "Recepcionista"
+
+# Estados de la habitación
+ESTADO_LIBRE = "Libre"
+ESTADO_OCUPADA = "Ocupada"
+ESTADO_SUCIA = "Sucia"
