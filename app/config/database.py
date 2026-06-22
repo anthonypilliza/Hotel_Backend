888cf86fdad5c876e352eb18d2da624726c2aa1d
anthonypilliza <anthonypilliza3@gmail.com>
2026-06-22 16:23:55 -0500

    configuracin final base de datos David Ramos

--- app/config/database.py
+++ app/config/database.py
@@ -1,9 +1,12 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
+from dotenv import load_dotenv
+import os
 # URL de conexión a MongoDB (local por defecto)
-MONGO_DETAILS = "mongodb://localhost:27017"
+load_dotenv()
+MONGO_DETAILS = os.getenv("MONGO_URL", "mongodb://localhost:27017")
 
 client = AsyncIOMotorClient(MONGO_DETAILS)
+
 
 # Nombre de la base de datos
 database = client.hotel_ups
